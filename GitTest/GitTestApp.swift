d937fc04f6b389bfa3f0f0037097eb66fe10e1b3
HyunJaeyeon <bomul0109@gmail.com>
2024-04-17 16:47:55 +0900

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Hyun Jaeyeon on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
